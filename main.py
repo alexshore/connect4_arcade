7ced3313fb9c2d8081e6b3f516be82b4df38a705
Alex Shore <alex@shore.me.uk>
2023-10-13 01:14:23 +0100

    removed poetry dependency

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import arcade
+
+
+SCREEN_SIZE = 720
+GRID_SIZE = 100
+BLOCK_SIZE = SCREEN_SIZE // GRID_SIZE
+
+
+class Connect4(arcade.Window):
+    def __init__(self) -> None:
+        super(Connect4, self).__init__(width=SCREEN_SIZE, height=SCREEN_SIZE)
+        arcade.set_background_color(arcade.color.SKY_BLUE)
