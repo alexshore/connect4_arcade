1ce41b4f547345bbe5c92a2619d48ed8f190c278
alex <kntrllr@gmail.com>
2023-10-27 23:41:45 +0100

    tidying & type annotations

--- main.py
+++ main.py
@@ -23,15 +23,16 @@ COLORS = {
 }
 
 """
-    thoughts:
-        - could have just used one class, didn't really need to separate game and board logic
-        - couple functions i don't really like:
-            - is_won_diagonally (could be made far more efficient i.e. not looping as many times by reducing start cell selections)
-            - make_move (uses hard to follow functions with unclear returns to make the move, fix in TODO)
-            - get_cell_of_mouse (yes it gets the cell the mouse is currently in but it doesn't take the coords explicitly)
-            - 
-
-
+thoughts:
+    - could have just used one class, didn't really need to separate game and board logic though it does allow for easy instant resetting
+    - couple functions i don't really like:
+        - is_won_diagonally (could be made far more efficient i.e. not looping as many times by reducing start cell possibilities)
+        - make_move (uses hard to follow functions with unclear returns to make the move, fix in TODO)
+    - need to actually add something saying the game is won/drawn
+    - could be interesting to also add a minmax algorithm driven opponent
+        - points? obv points to win but also points for a threat of a win? 
+        - should create a positions dict so not to calculate the same position multiple times
+        - prune any branches that instantly give the opponent a win
 """
 
 
@@ -40,16 +41,16 @@ class Game:
         self.grid = np.full(GRID_SHAPE, Checker.NONE)
         self.side = Checker.PRIMARY
 
-    def switch_side(self):
+    def switch_side(self) -> None:
         self.side = Checker(-self.side.value)
 
-    def is_cell_in_grid(self, cell):
+    def is_cell_in_grid(self, cell: tuple[int, int]) -> bool:
         return 0 <= cell[0] < GRID_SHAPE[0] and 0 <= cell[1] < GRID_SHAPE[1]
 
-    def is_drawn(self):
+    def is_drawn(self) -> bool:
         return np.where(self.grid == Checker.NONE)[0].size == 0
 
-    def is_won_diagonally(self):
+    def is_won_diagonally(self) -> bool:
         # up-right
         for i in range(-GRID_SHAPE[1], GRID_SHAPE[0]):
             count = 0
@@ -86,7 +87,7 @@ class Game:
 
         return False
 
-    def is_won_vertically(self):
+    def is_won_vertically(self) -> bool:
         for i in range(GRID_SHAPE[0]):
             count = 0
 
@@ -101,7 +102,7 @@ class Game:
 
         return False
 
-    def is_won_horizontally(self):
+    def is_won_horizontally(self) -> bool:
         for j in range(GRID_SHAPE[1]):
             count = 0
 
@@ -116,14 +117,14 @@ class Game:
 
         return False
 
-    def is_won(self):
+    def is_won(self) -> bool:
         return self.is_won_horizontally() or self.is_won_vertically() or self.is_won_diagonally()
 
-    def is_valid_move(self, column):
+    def is_valid_move(self, column: int) -> bool:
         # checks for at least 1 cell with a Checker.NONE in it
         return np.count_nonzero(self.grid[column] == Checker.NONE) != 0
 
-    def make_move(self, column: int):
+    def make_move(self, column: int) -> None:
         if not self.is_valid_move(column):
             return
 
@@ -136,18 +137,18 @@ class Game:
 class Connect4(arcade.Window):
     def __init__(self) -> None:
         super(Connect4, self).__init__(width=GRID_WIDTH, height=GRID_HEIGHT)
-        arcade.set_background_color(arcade.color_from_hex_string("393E41"))
+        arcade.set_background_color((57, 62, 65))
 
-    def setup(self):
+    def setup(self) -> None:
         self.game = Game()
 
         self.paused = False
         self.mouse_x = 0
         self.mouse_y = 0
 
-    # --- event methods ---
+    # --- overloaded methods ---
 
-    def on_draw(self):
+    def on_draw(self) -> None:
         self.clear()
         for i in range(GRID_SHAPE[0]):
             for j in range(GRID_SHAPE[1]):
@@ -156,19 +157,18 @@ class Connect4(arcade.Window):
         if not self.paused:
             self.draw_mouse_hover()
 
-    def on_key_press(self, symbol: int, modifiers: int):
+    def on_key_press(self, symbol: int, modifiers: int) -> None:
         match symbol:
             case arcade.key.Q:
                 arcade.exit()
             case arcade.key.SPACE:
                 self.setup()
 
-    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int) -> None:
         self.mouse_x = x
         self.mouse_y = y
 
-    # i don't like how i need to take x,y as args when they're not used but thats the arcade api
-    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int) -> None:
         if self.paused or button != arcade.MOUSE_BUTTON_LEFT:
             return
 
@@ -180,20 +180,20 @@ class Connect4(arcade.Window):
 
         self.game.switch_side()
 
-    # --- misc methods ---
+    # --- custom methods ---
 
-    def get_cell_of_mouse(self):
+    def get_cell_of_mouse(self) -> tuple[int, int]:
         return self.mouse_x // CELL_SIZE, self.mouse_y // CELL_SIZE
 
-    def get_centre_of_cell(self, cell):
+    def get_centre_of_cell(self, cell: tuple[int, int]) -> tuple[int, int]:
         return (cell[0] * CELL_SIZE) + (CELL_SIZE // 2), (cell[1] * CELL_SIZE) + (CELL_SIZE // 2)
 
-    def is_mouse_inside_checker(self, center_x, center_y):
+    def is_mouse_inside_checker(self, center_x: int, center_y: int) -> bool:
         # check if distance from center of circle to mouse (using pythag) is less than radius
         # https://math.stackexchange.com/questions/198764/how-to-know-if-a-point-is-inside-a-circle
         return (self.mouse_x - center_x) ** 2 + (self.mouse_y - center_y) ** 2 <= CHECKER_RADIUS**2
 
-    def draw_checker(self, cell):
+    def draw_checker(self, cell: tuple[int, int]) -> None:
         center_x, center_y = self.get_centre_of_cell(cell)
         arcade.draw_circle_filled(
             center_x=center_x,
@@ -202,7 +202,7 @@ class Connect4(arcade.Window):
             color=COLORS[self.game.grid[cell]],
         )
 
-    def draw_mouse_hover(self):
+    def draw_mouse_hover(self) -> None:
         cell = self.get_cell_of_mouse()
 
         if not self.game.is_cell_in_grid(cell):
@@ -224,7 +224,7 @@ class Connect4(arcade.Window):
         )
 
 
-def main():
+def main() -> None:
     game = Connect4()
     game.setup()
     arcade.run()
